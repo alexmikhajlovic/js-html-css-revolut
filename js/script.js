024b52b991fec4e88c741c1a35c4111239a8c1cd
Alex Greco <alexmikhajlovic@gmail.com>
2021-01-12 16:53:14 +0100

    Work in progress

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     
+    $('nav.container ul li.has_sub_menu').hover(function(){
+        $(this).find('.dropdown_menu').toggleClass('active');
+    });
 
-
-    
 });
