118e87f931ff754d3bcc7fd316bf5e41a24c98c9
Alex Greco <alexmikhajlovic@gmail.com>
2021-01-12 18:15:33 +0100

    End

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     
+    // ! Open DropDown Menu on hover
     $('nav.container ul li.has_sub_menu').hover(function(){
         $(this).find('.dropdown_menu').toggleClass('active');
     });
-
+    
 });
